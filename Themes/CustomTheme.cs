e63e9e370710c87a7862e40e0e297e160e3ae2b5
Dresnclor <DRESNCLOR@QQ.COM>
2024-04-07 23:50:01 +0800

    添加自定义主题类

---
+++ Themes/CustomTheme.cs
@@ -0,0 +1,15 @@
+namespace WinDocking.Docking
+{
+    using Themes;
+
+    /// <summary>
+    /// 自定义外部主题
+    /// </summary>
+    public class CustomTheme : VThemeBase
+    {
+        public CustomTheme(string file)
+            : base(file)
+        {
+        }
+    }
+}
--- Themes/VThemeBase.cs
+++ Themes/VThemeBase.cs
@@ -1,44 +1,75 @@
 namespace WinDocking.Themes
 {
-    using Docking;
+	using Docking;
+	using System.IO;
 
-    /// <summary>
-    /// Visual Studio 2015 theme base.
-    /// </summary>
-    public abstract class VThemeBase : ThemeBase
-    {
-        public VThemeBase(byte[] resources)
-        {
-            ColorPalette = new DockPanelColorPalette(new PaletteFactory(resources));
-            Skin = new DockPanelSkin();
-            PaintingService = new PaintingService();
-            ImageService = new ImageService(this);
-            ToolStripRenderer = new VToolStripRenderer(ColorPalette)
-            {
-                UseGlassOnMenuStrip = false,
-            };
-            Measures.SplitterSize = 6;
-            Measures.AutoHideSplitterSize = 3;
-            Measures.DockPadding = 6;
-            ShowAutoHideContentOnHover = false;
-            Extender.AutoHideStripFactory = new AutoHideStripFactory();
-            Extender.AutoHideWindowFactory = new AutoHideWindowFactory();
-            Extender.DockPaneFactory = new DockPaneFactory();
-            Extender.DockPaneCaptionFactory = new DockPaneCaptionFactory();
-            Extender.DockPaneStripFactory = new DockPaneStripFactory();
-            Extender.DockPaneSplitterControlFactory = new DockPaneSplitterControlFactory();
-            Extender.WindowSplitterControlFactory = new WindowSplitterControlFactory();
-            Extender.DockWindowFactory = new DockWindowFactory();
-            Extender.PaneIndicatorFactory = new PaneIndicatorFactory();
-            Extender.PanelIndicatorFactory = new PanelIndicatorFactory();
-            Extender.DockOutlineFactory = new DockOutlineFactory();
-            Extender.DockIndicatorFactory = new DockIndicatorFactory();
-        }
+	/// <summary>
+	/// Visual Studio 2015 theme base.
+	/// </summary>
+	public abstract class VThemeBase : ThemeBase
+	{
+		public VThemeBase(byte[] resources)
+		{
+			ColorPalette = new DockPanelColorPalette(new PaletteFactory(resources));
+			Skin = new DockPanelSkin();
+			PaintingService = new PaintingService();
+			ImageService = new ImageService(this);
+			ToolStripRenderer = new VToolStripRenderer(ColorPalette)
+			{
+				UseGlassOnMenuStrip = false,
+			};
+			Measures.SplitterSize = 6;
+			Measures.AutoHideSplitterSize = 3;
+			Measures.DockPadding = 6;
+			ShowAutoHideContentOnHover = false;
+			Extender.AutoHideStripFactory = new AutoHideStripFactory();
+			Extender.AutoHideWindowFactory = new AutoHideWindowFactory();
+			Extender.DockPaneFactory = new DockPaneFactory();
+			Extender.DockPaneCaptionFactory = new DockPaneCaptionFactory();
+			Extender.DockPaneStripFactory = new DockPaneStripFactory();
+			Extender.DockPaneSplitterControlFactory = new DockPaneSplitterControlFactory();
+			Extender.WindowSplitterControlFactory = new WindowSplitterControlFactory();
+			Extender.DockWindowFactory = new DockWindowFactory();
+			Extender.PaneIndicatorFactory = new PaneIndicatorFactory();
+			Extender.PanelIndicatorFactory = new PanelIndicatorFactory();
+			Extender.DockOutlineFactory = new DockOutlineFactory();
+			Extender.DockIndicatorFactory = new DockIndicatorFactory();
+		}
 
-        public override void CleanUp(DockPanel dockPanel)
-        {
-            PaintingService.CleanUp();
-            base.CleanUp(dockPanel);
-        }
-    }
+		public VThemeBase(string file)
+		{
+			byte[] bs = File.ReadAllBytes(file);
+			ColorPalette = new DockPanelColorPalette(new PaletteFactory(bs));
+			Skin = new DockPanelSkin();
+			PaintingService = new PaintingService();
+			ImageService = new ImageService(this);
+			ToolStripRenderer = new VToolStripRenderer(ColorPalette)
+			{
+				UseGlassOnMenuStrip = false,
+			};
+			Measures.SplitterSize = 6;
+			Measures.AutoHideSplitterSize = 3;
+			Measures.DockPadding = 6;
+			ShowAutoHideContentOnHover = false;
+			Extender.AutoHideStripFactory = new AutoHideStripFactory();
+			Extender.AutoHideWindowFactory = new AutoHideWindowFactory();
+			Extender.DockPaneFactory = new DockPaneFactory();
+			Extender.DockPaneCaptionFactory = new DockPaneCaptionFactory();
+			Extender.DockPaneStripFactory = new DockPaneStripFactory();
+			Extender.DockPaneSplitterControlFactory = new DockPaneSplitterControlFactory();
+			Extender.WindowSplitterControlFactory = new WindowSplitterControlFactory();
+			Extender.DockWindowFactory = new DockWindowFactory();
+			Extender.PaneIndicatorFactory = new PaneIndicatorFactory();
+			Extender.PanelIndicatorFactory = new PanelIndicatorFactory();
+			Extender.DockOutlineFactory = new DockOutlineFactory();
+			Extender.DockIndicatorFactory = new DockIndicatorFactory();
+		}
+
+
+		public override void CleanUp(DockPanel dockPanel)
+		{
+			PaintingService.CleanUp();
+			base.CleanUp(dockPanel);
+		}
+	}
 }
